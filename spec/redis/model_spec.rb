5141f0a3febd703c12ba3a551e9e356d2b4b473f
Blake Imsland <blake@retroco.de>
2010-06-28 01:53:03 -0700

    Made spacing consistant across files

--- spec/redis/model_spec.rb
+++ spec/redis/model_spec.rb
@@ -3,31 +3,30 @@ require 'redis/model'
 require 'json'
 
 describe Redis::Model do
-  
   context "DSL" do
     class TestDSL < Redis::Model
       field :foo
       list  :bar
       set   :sloppy
     end
-  
+
     before(:each) do
       @x = TestDSL.with_key(1)
     end
-  
+
     it "should define rw accessors for field" do
       @x.should respond_to(:foo)
       @x.should respond_to(:foo=)
     end
-  
+
     it "should define r accessor for list" do
       @x.should respond_to(:bar)
     end
-  
+
     it "should define r accessor for set" do
       @x.should respond_to(:sloppy)
     end
-  
+
     it "should raise error on invalid type" do
       lambda do
         class TestInvalidType < Redis::Model
@@ -36,7 +35,7 @@ describe Redis::Model do
       end.should raise_error(ArgumentError, 'Unknown type invalid_type for field invalid')
     end
   end
-  
+
   context "field type cast" do
     class TestType < Redis::Model
       field :foo_string, :string
@@ -44,11 +43,11 @@ describe Redis::Model do
       field :foo_date,   :datetime
       field :foo_int,    :int
       field :foo_float,  :float
-      
+
       list  :list_date,  :datetime
       set   :set_date,   :datetime
     end
-  
+
     before(:each) do
       @xRedisMock = Spec::Mocks::Mock.new
       @yRedisMock = Spec::Mocks::Mock.new
@@ -57,14 +56,14 @@ describe Redis::Model do
       @x.stub!(:redis).and_return(@xRedisMock)
       @y.stub!(:redis).and_return(@yRedisMock)
     end
-  
+
     it "should save string as is" do
       @xRedisMock.should_receive(:[]=).with('test_type:1:foo_string', 'xxx')
       @yRedisMock.should_receive(:[]).with('test_type:1:foo_string').and_return('xxx')
       @x.foo_string = 'xxx'
       @y.foo_string.should be_instance_of(String)
     end
-  
+
     it "should marshal integer fields" do
       @xRedisMock.should_receive(:[]=).with('test_type:1:foo_int', '12')
       @yRedisMock.should_receive(:[]).with('test_type:1:foo_int').and_return('12')
@@ -72,7 +71,7 @@ describe Redis::Model do
       @y.foo_int.should be_kind_of(Integer)
       @y.foo_int.should == 12
     end
-  
+
     it "should marshal float fields" do
       @xRedisMock.should_receive(:[]=).with('test_type:1:foo_float', '12.1')
       @yRedisMock.should_receive(:[]).with('test_type:1:foo_float').and_return('12.1')
@@ -80,7 +79,7 @@ describe Redis::Model do
       @y.foo_float.should be_kind_of(Float)
       @y.foo_float.should == 12.1
     end
-  
+
     it "should marshal datetime fields" do
       time = DateTime.now
       str  = time.strftime('%FT%T%z')
@@ -90,7 +89,7 @@ describe Redis::Model do
       @y.foo_date.should be_kind_of(DateTime)
       @y.foo_date.should.to_s == time.to_s
     end
-  
+
     it "should marshal json structs" do
       data = {'foo' => 'bar', 'x' => 2}
       str  = JSON.dump(data)
@@ -100,16 +99,16 @@ describe Redis::Model do
       @y.foo_json.should be_kind_of(Hash)
       @y.foo_json.should.inspect == data.inspect
     end
-    
+
     it "should return nil for empty fields" do
       @xRedisMock.should_receive(:[]).with('test_type:1:foo_date').and_return(nil)
       @x.foo_date.should be_nil
     end
-    
+
     it "should marshal list values" do
       data = DateTime.now
       str  = data.strftime('%FT%T%z')
-      
+
       @xRedisMock.should_receive('rpush').with('test_type:1:list_date', str)
       @xRedisMock.should_receive('lset').with('test_type:1:list_date', 1, str)
       @xRedisMock.should_receive('exists').with('test_type:1:list_date', str)
@@ -129,7 +128,7 @@ describe Redis::Model do
       @x.list_date.pop_tail.should be_kind_of(DateTime)
       @x.list_date.pop_head.should be_kind_of(DateTime)
     end
-    
+
     it "should marshal set values" do
       data = DateTime.now
       str  = data.strftime('%FT%T%z')
@@ -151,30 +150,30 @@ describe Redis::Model do
     end
 
   end
-  
+
   context "increment/decrement" do
     class TestIncrements < Redis::Model
       field :foo, :integer
       field :bar, :string
       field :baz, :float
     end
-    
+
     before do
       @redisMock = Spec::Mocks::Mock.new
       @x = TestIncrements.with_key(1)
       @x.stub!(:redis).and_return(@redisMock)
     end
-    
+
     it "should send INCR when #increment! is called on an integer" do
       @redisMock.should_receive(:incr).with("test_increments:1:foo", 1)
       @x.increment!(:foo)
     end
-    
+
     it "should send DECR when #decrement! is called on an integer" do
       @redisMock.should_receive(:decr).with("test_increments:1:foo", 1)
       @x.decrement!(:foo)
     end
-    
+
     it "should raise an ArgumentError when called on non-integers" do
       [:bar, :baz].each do |f|
         lambda{@x.increment!(f)}.should raise_error(ArgumentError)
@@ -182,45 +181,45 @@ describe Redis::Model do
       end
     end
   end
-  
+
   context "redis commands" do
     class TestCommands < Redis::Model
       field :foo
       list  :bar
       set   :sloppy
     end
-    
+
     before(:each) do
       @redisMock = Spec::Mocks::Mock.new
       @x = TestCommands.with_key(1)
       @x.stub!(:redis).and_return(@redisMock)
     end
-  
+
     it "should send GET on field read" do
       @redisMock.should_receive(:[]).with('test_commands:1:foo')
       @x.foo
     end
-  
+
     it "should send SET on field write" do
       @redisMock.should_receive(:[]=).with('test_commands:1:foo', 'bar')
       @x.foo = 'bar'
     end
-  
+
     it "should send RPUSH on list <<" do
       @redisMock.should_receive(:rpush).with('test_commands:1:bar', 'bar')
       @x.bar << 'bar'
     end
-  
+
     it "should send SADD on set <<" do
       @redisMock.should_receive(:sadd).with('test_commands:1:sloppy', 'bar')
       @x.sloppy << 'bar'
     end
-    
+
     it "should delete separate fields" do
       @redisMock.should_receive(:delete).with('test_commands:1:foo')
       @x.delete :foo
     end
-    
+
     it "should delete all field" do
       @redisMock.should_receive(:delete).with('test_commands:1:foo')
       @redisMock.should_receive(:delete).with('test_commands:1:bar')
